22f2a3027a4dc96863cb7ed950679f5ac6847205
RoyOkite <royokite@gmail.com>
2022-08-09 22:46:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Your code here
+// Acting on events lab!
+
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    const leftInt = dodger.style.left.replace('px', '');
+    const left = parseInt(leftInt, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftInt = dodger.style.left.replace('px', '');
+    const left = parseInt(leftInt, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key==='ArrowLeft') {
+        moveDodgerLeft();
+    } else if (event.key==='ArrowRight') {
+        moveDodgerRight();
+    }
+})
